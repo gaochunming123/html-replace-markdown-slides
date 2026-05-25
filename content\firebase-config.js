10f3e8293afbac6ad445a7788c192bcda80e3668
gaochunming123 <108186178+gaochunming123@users.noreply.github.com>
2026-05-25 11:23:20 +0800

    Add content\firebase-config.js

---
+++ content\firebase-config.js
@@ -0,0 +1,10 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+window.WORDCLOUD_COLLECTION = "html-markdown-wordcloud";
